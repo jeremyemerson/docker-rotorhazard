009f938df0f007e8b92170cbc4de43e1a727d59a
Jeremy Emerson <jeremy.emerson@gmail.com>
2023-05-29 18:55:27 -0400

    moved stuff

--- init.sh
+++
@@ -1,9 +0,0 @@
-# move files
-cp -R /tmp/rotorhazard/RotorHazard /opt/rotorhazard \
-
-# remove temp files
-# rm -rf /tmp/rotorhazard/RotorHazard
-
-# start server
-cd /opt/rotorhazard/RotorHazard/src/server
-python server.py
